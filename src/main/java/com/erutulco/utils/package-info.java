493a9167e14fcd2c2649602915111fc9cbca1761
Joaquin Gatica <joaquingatica@gmail.com>
2017-02-14 10:18:26 -0300

    Add package-info.java file

---
+++ src/main/java/com/erutulco/utils/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * Imladris Calendar
+ * <p>
+ * Copyright 2012 Joaquín Gatica (Erutulco Eruntano)
+ * <p>
+ * Contact:
+ * Twitter: <http://twitter.com/joaquingatica>
+ * Email: <erutulco@quenya101.com>
+ * <p>
+ * This file is part of "Imladris Calendar".
+ * <p>
+ * "Imladris Calendar" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * "Imladris Calendar" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with "Imladris Calendar".  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @since 1.0.0
+ * @author Joaquín Gatica<erutulco@quenya101.com>
+ * @version 1.2.1
+ */
+package com.erutulco.utils;
